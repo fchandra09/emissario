ee3e5050ca510b7ee8cc00312d2327720c93966a
Felicia Chandra <fchandr2@illinois.edu>
2015-11-28 16:22:51 -0600

    Updated error page.

--- application/views/error/index.php
+++ application/views/error/index.php
@@ -1,5 +1,8 @@
 <?php if (!$this) { exit(header('HTTP/1.0 403 Forbidden')); } ?>
 
 <div class="container">
-    <p>This is the Error-page. Will be shown when a page (= controller / method) does not exist.</p>
+	<div class="alert alert-danger" role="alert">
+		<b>ERROR:</b>
+		The page you are trying to reach does not exist.
+	</div>
 </div>
