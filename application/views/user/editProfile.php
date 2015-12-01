84d62f566b60d3ac2c41026e56a644643327a333
Felicia Chandra <fchandr2@illinois.edu>
2015-12-01 10:19:48 -0600

    Updated titles to ensure they are consistent across the whole system.

--- application/views/user/editProfile.php
+++ application/views/user/editProfile.php
@@ -20,7 +20,7 @@ $phone = $user->Phone;
 </script>
 
 <div class="container">
-	<h2 class="page-header">Edit Profile</h2>
+	<h2 class="page-header">Edit User Details</h2>
 	<form id="form" method="post" action="<?php echo URL_WITH_INDEX_FILE; ?>user/saveProfile" class="form-horizontal">
 		<input type="hidden" id="userID" name="userID" value="<?php echo $userID ?>" />
 
--- application/views/user/index.php
+++ application/views/user/index.php
@@ -56,7 +56,7 @@
 		</div>
 		<div class="form-group">
 			<div class="col-sm-offset-2 col-sm-10">
-				<button type="button" id="editProfile" class="btn btn-default">Edit Profile</button>
+				<button type="button" id="editProfile" class="btn btn-default">Edit User Details</button>
 			</div>
 		</div>
 	</div>
